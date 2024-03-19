a5ff9f8d068a77b7a8eb470f56c1581fad96b77a
m020202 <68419544+m020202@users.noreply.github.com>
2024-03-20 00:08:02 +0900

    Update [코드트리] n* n 표 채우기.py

--- [코드트리] n* n 표 채우기.py
+++ [코드트리] n* n 표 채우기.py
@@ -1,4 +1,5 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 n = int(input())
 m = int(input())
@@ -7,12 +8,15 @@ box = [[0] * n for _ in range(n)]
 dx = [-1,0,1,0]
 dy = [0,1,0,-1]
 cur = 0
-
 ansX = 0
 ansY = 0
 
-def dfs(x,y,dir,cnt):
-    global ansX, ansY
+box[n//2][n//2] = 1
+
+dQ = deque()
+dQ.append((n//2-1, n//2, 0, 2))
+while dQ:
+    x,y,dir,cnt = dQ.popleft()
     box[x][y] = cnt
     if cnt == m:
         ansX = x
@@ -23,13 +27,12 @@ def dfs(x,y,dir,cnt):
     yy = y + dy[nextDir]
     if 0<=xx<n and 0<=yy<n:
         if (box[xx][yy] == 0):
-            return dfs(xx,yy,nextDir,cnt+1)
+            dQ.append((xx,yy,nextDir,cnt+1))
         else:
             if 0<=x+dx[dir]<n and 0<=y+dy[dir]<n:
-                return dfs(x + dx[dir], y + dy[dir], dir, cnt+1)
+                dQ.append((x + dx[dir], y + dy[dir], dir, cnt+1))
 
-box[n//2][n//2] = 1
-dfs(n//2-1, n//2, 0, 2)
+
 for i in range(n):
     for j in range(n):
         print(box[i][j], end=' ')
