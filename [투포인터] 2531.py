91668cce7c1f3222d7915633eb0c7f2a5baca343
m020202 <68419544+m020202@users.noreply.github.com>
2024-03-25 23:31:57 +0900

    Update [투포인터] 2531.py
    슬라이스 사용해서 구현

--- [투포인터] 2531.py
+++ [투포인터] 2531.py
@@ -3,34 +3,20 @@ from collections import deque
 input = sys.stdin.readline
 n,d,k,c = map(int,input().split())
 box = [int(input()) for _ in range(n)]
-def checking(lt, rt):
-    if lt > rt:
-        for i in range(lt, n):
-            if box[i] == c:
-                return 0
-        for i in range(0, rt + 1):
-            if box[i] == c:
-                return 0
+lt = 0
+rt = (k-1) % n
+ch = [0] * n
+ans = 0
+while (ch[lt] == 0):
+    if lt < rt:
+        cur = box[lt:rt+1]
     else:
-        for i in range(lt, rt + 1):
-            if box[i] == c:
-                return 0
-    return 1
-
-cur = deque([])
-for i in range(0,k):
-    cur.append(box[i])
-ans = (len(set(cur))) + checking(0,k-1)
-
-lt = 1
-rt = k % n
-while (lt != 0):
-    cur.popleft()
-    cur.append(box[rt])
+        cur = box[lt:] + box[:rt+1]
+    cur += [c]
     dist = set(cur)
-    cnt = (checking(lt, rt) + len(dist))
-    ans = max(ans,cnt)
-    lt  = (lt + 1) % n
-    rt = (lt + k-1) % n
+    ans = max(ans, len(dist))
+    ch[lt] = 1
+    lt = (lt + 1) % n
+    rt = (lt + k - 1) % n
 
 print(ans)
