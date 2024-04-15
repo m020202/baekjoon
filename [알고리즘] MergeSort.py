91db04e12d211d0619e2f88e4cc8b3cf23f9d383
m020202 <68419544+m020202@users.noreply.github.com>
2024-04-15 18:22:20 +0900

    Update [알고리즘] MergeSort.py

--- [알고리즘] MergeSort.py
+++ [알고리즘] MergeSort.py
@@ -1,39 +1,43 @@
-from collections import deque
 E = [5,4,3,2,1]
+srt = [0] * len(E)
 
-def merge(f,mid,l):  
-    global E
-    left = E[f:mid+1]
-    right = E[mid+1:l+1]
-    left = deque(left)
-    right = deque(right)
+def merge(E,f,mid,l):
+    i = f
+    j = mid + 1
     cur = f
-
-    while (cur < l+1):
-        if len(left) == 0:
-            E[cur] = right.popleft()
+    while(i <= mid and j <= l):
+        if E[i] <= E[j]:
+            srt[cur] = E[i]
+            i += 1
             cur += 1
-            continue
-        elif len(right) == 0:
-            E[cur] = left.popleft()
-            cur += 1
-            continue
-
-        if left[0] < right[0]:
-            E[cur] = left.popleft()
         else:
-            E[cur] = right.popleft()
-        cur += 1
-    print(E)
-    return 
+            srt[cur] = E[j]
+            j += 1
+            cur += 1
+    
+    if i <= mid:
+        rng = mid - i + 1
+        for k in range(0,rng):
+            srt[cur] = E[i]
+            cur += 1
+            i += 1
+    if j < l:
+        rng = l - j + 1
+        for k in range(0,rng):
+            srt[cur] = E[j]
+            cur += 1
+            j += 1
+    
+    for i in range(f,l+1):
+        E[i] = srt[i]
 
-def mergeSort(f,l):
-    global E
+def mergeSort(E,f,l):
     if f < l:
-        mid = (f+l)//2
-        mergeSort(f,mid)
-        mergeSort(mid+1,l)
-        merge(f,mid,l)
-mergeSort(0,4)
+        mid = (f + l) // 2
+        mergeSort(E,f,mid)
+        mergeSort(E,mid+1,l)
+        merge(E,f,mid,l)
+        return
+    
+mergeSort(E,0,4)
 print(E)
-
